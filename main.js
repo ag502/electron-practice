38d50ae455888e637240c83662b0ecff1eba57e8
ag502 <ag502@naver.com>
2022-11-07 23:17:56 +0900

    add: app API

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // Modules
 const { app, BrowserWindow } = require("electron");
 
+setTimeout(() => {
+  console.log("Checking ready: " + app.isReady());
+}, 2000);
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
@@ -31,8 +35,30 @@ function createWindow() {
   });
 }
 
+app.on("before-quit", (e) => {
+  console.log("App is quitting");
+  e.preventDefault();
+});
+
 // Electron `app` is ready
-app.on("ready", createWindow);
+app.on("ready", () => {
+  console.log("App is ready!");
+  console.log(app.getPath("desktop"));
+  console.log(app.getPath("music"));
+  console.log(app.getPath("userData"));
+  createWindow();
+});
+
+app.on("browser-window-blur", (e) => {
+  console.log("app unfocused");
+  setTimeout(() => {
+    app.quit();
+  }, 3000);
+});
+
+app.on("browser-window-focus", () => {
+  console.log("app focus");
+});
 
 // Quit when all windows are closed - (Not macOS - Darwin)
 app.on("window-all-closed", () => {
