6ae367f74203cc7965b9522cb2b69f08a20fb81f
ag502 <ag502@naver.com>
2022-11-08 22:28:01 +0900

    add: create browser window

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ const { app, BrowserWindow } = require("electron");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow, secondaryWindow;
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
+    frame: false,
+    titleBarStyle: "hidden",
     webPreferences: {
       // --- !! IMPORTANT !! ---
       // Disable 'contextIsolation' to allow 'nodeIntegration'
@@ -17,18 +19,53 @@ function createWindow() {
       contextIsolation: false,
       nodeIntegration: true,
     },
+    // to prevent flicker
+    // backgroundColor: "#2b2e3b",
+    // show: false,
   });
+
+  // child window
+  // secondaryWindow = new BrowserWindow({
+  //   width: 600,
+  //   height: 300,
+  //   webPreferences: {
+  //     // --- !! IMPORTANT !! ---
+  //     // Disable 'contextIsolation' to allow 'nodeIntegration'
+  //     // 'contextIsolation' defaults to "true" as from Electron v12
+  //     contextIsolation: false,
+  //     nodeIntegration: true,
+  //   },
+  //   parent: mainWindow,
+  //   modal: true,
+  //   show: false,
+  // });
 
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile("index.html");
+  // secondaryWindow.loadFile("secondary.html");
 
-  // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  // setTimeout(() => {
+  //   secondaryWindow.show();
+  //   setTimeout(() => {
+  //     secondaryWindow.close();
+  //     secondaryWindow = null;
+  //   }, 3000);
+  // }, 2000);
+
+  // load outter url
+  //   mainWindow.loadURL("https://naver.com");
+
+  // show after load finish
+  //   mainWindow.once("ready-to-show", mainWindow.show);
 
   // Listen for window being closed
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
+
+  // secondaryWindow.on("closed", () => {
+  //   secondaryWindow = null;
+  // });
 }
 
 // Electron `app` is ready
