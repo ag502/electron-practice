4ea88115d930149328b44d230096810daca2c8bf
ag502 <ag502@naver.com>
2022-11-26 14:01:52 +0900

    add: offscreen rendering

--- main.js
+++ main.js
@@ -1,34 +1,57 @@
 // Modules
 const { app, BrowserWindow } = require("electron");
+const fs = require("fs");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+
+app.disableHardwareAcceleration();
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
+    show: false,
     webPreferences: {
       // --- !! IMPORTANT !! ---
       // Disable 'contextIsolation' to allow 'nodeIntegration'
       // 'contextIsolation' defaults to "true" as from Electron v12
       contextIsolation: false,
       nodeIntegration: true,
+      offscreen: true,
     },
   });
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile("index.html");
+  // mainWindow.loadFile("index.html");
+  mainWindow.loadURL("https://electronjs.org");
 
-  // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  let i = 1;
+  mainWindow.webContents.on("paint", (e, dirty, image) => {
+    let screenshot = image.toPNG();
+    fs.writeFile(
+      app.getPath("desktop") + `/screenshot_${i}.png`,
+      screenshot,
+      console.log
+    );
+    i++;
+  });
 
-  // Listen for window being closed
-  mainWindow.on("closed", () => {
+  mainWindow.webContents.on("did-finish-load", (e) => {
+    console.log(mainWindow.getTitle());
+    mainWindow.close();
     mainWindow = null;
   });
+
+  // Open DevTools - Remove for PRODUCTION!
+  // mainWindow.webContents.openDevTools();
+
+  // Listen for window being closed
+  // mainWindow.on("closed", () => {
+  //   mainWindow = null;
+  // });
 }
 
 // Electron `app` is ready
